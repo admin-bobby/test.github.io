3074773e15aecdcb157d85f3d0b91c5570aca33d
admin-bobby <bobbymittal868481@gmail.com>
2023-06-14 13:25:23 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">	
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<section class="navibar">
+<nav class="navbar">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#"><img src="./image/Rectangle.png" class="img-responsive"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navigation">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown">
+        </li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Services</a></li>
+		<li><a href="#">Contact Us</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right" id="navi">
+        <li><a href="#" class="btn"><span class="glyphicon glyphicon-user"></span> Log In/Sign Out</a></li>
+        <li><a href="#" class="btn" id="btn-1"><span class="glyphicon glyphicon-log-in"></span> Download App</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</section>
+
+
+<!---
+<section class="navibar">
+<div class="container">
+<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+<a href="#"><img src="./image/Rectangle.png" class="img-responsive"></a>
+</div>
+<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">Contact Us</a></li>
+</ul>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" id="navi">
+<a href="#" class="btn">Log In/Sign Out</a>
+<a href="#" class="btn" id="btn-1">Download App <i class="fa fa-download" aria-hidden="true"></i></a>
+</div>
+</div>
+</section> --->
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,244 @@
+<?php
+include('header.php');
+?>
+<section class="banner">
+<div class="container-fluid" style="padding:0;">
+<img class="img-responsive" src="./image/Banner.png" width="100%">
+</div>
+</section>
+<section class="location">
+<div class="container">
+<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+<h1>Your Easiest Way<br>to Deliver!!!</h1>
+<p>- Fast intracity courier delivery service</p>
+<a class="btn" href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> Pick-up Address </a>
+<a class="btn" href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> Drop off Address </a>
+<button>Calculate Price</button>
+<div>
+</div>
+</section>
+
+<section class="pick">
+<div class="container">
+<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/Vector.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h3>Request</h3>
+<p>Fill in the details and submit the request. </p>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/Vector2.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h3>Pick up</h3>
+<p>After confirm the request our againt will pick the package.</p>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/Vector1.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h3>Deliver</h3>
+<p>Get notifications once it has been delivered.</p>
+</div>
+</div>
+</div>
+</section>
+<section class="about">
+<div class="container">
+<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+<img src="./image/Service-Side-image.png" class="img-responsive">
+</div>
+<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+<h2>Our Services</h2>
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="item">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/vector3.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h4>Warehousing</h4>
+<p>Non-Air Con / Air Con</p>
+</div>
+</div>
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="item">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/vector4.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h4>Local Courier Services </h4>
+<p>Documents / Small Parcels</p>
+</div>
+</div>
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="item">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/vector5.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h4>Local Distribution</h4>
+<p>Loose Cartons / Pallets</p>
+</div>
+</div>
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="item">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/vector6.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h4>Freight Forwarding</h4>
+<p>Import / Export</p>
+</div>
+</div>
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="item">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/vector7.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h4>Home Delivery</h4>
+<p>Fast Deliver Service</p>
+</div>
+</div>
+</div>
+
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="item">
+<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+<img src="./image/vector8.png" class="img-responsive">
+</div>
+<div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+<h4>Pick & Pack</h4>
+<p>Providing Esteemed Service</p>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+</section>
+
+<section class="test">
+<div class="container">
+<div class="col-lg-1 col-md-1 hidden-sm hidden-xs"></div>
+<div class="col-lg-10 col-md-10 col-sm-12 col-xs-12" style="background:#F7EFFF; padding:50px;">
+<h2>Testimonal</h2>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+  <div class="carousel-inner">
+    <div class="item active">
+      <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+	  <img src="./image/test-Image.png" class="img-responsive">
+	  </div>
+	  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+	  <img src="./image/vector9.png" class="img-responsive" width="51">
+	  <p>“One of the benefits of using is I can monitor all my shipments easily in one place. Also we don't have to write air waybills anymore. We can key in all the information, so my colleagues don't have to use the time to fill in all the forms again.”</p>
+	  <h4>Sam Khong</h4>
+	  <span>-CEO of Company</span>
+	  </div>
+    </div>
+
+    <div class="item">
+      <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+	  <img src="./image/test-Image.png" class="img-responsive">
+	  </div>
+	  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+	  <img src="./image/vector9.png" class="img-responsive" width="51">
+	  <p>“One of the benefits of using is I can monitor all my shipments easily in one place. Also we don't have to write air waybills anymore. We can key in all the information, so my colleagues don't have to use the time to fill in all the forms again.”</p>
+	  <h4>Sam Khong</h4>
+	  <span>-CEO of Company</span>
+	  </div>
+    </div>
+
+    <div class="item">
+      <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+	  <img src="./image/test-Image.png" class="img-responsive">
+	  </div>
+	  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+	  <img src="./image/vector9.png" class="img-responsive" width="51">
+	  <p>“One of the benefits of using is I can monitor all my shipments easily in one place. Also we don't have to write air waybills anymore. We can key in all the information, so my colleagues don't have to use the time to fill in all the forms again.”</p>
+	  <h4>Sam Khong</h4>
+	  <span>-CEO of Company</span>
+	  </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" style="width:0px;" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" style="width:0px;" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<div class="col-lg-1 col-md-1 hidden-sm hidden-xs"></div>
+</div>
+</section>
+
+<section class="pickup">
+<div class="container">
+<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+<h2>We Provide Pickup & Delivery Custom Time You Want...!!</h2>
+</div>
+<div class="col-lg-1 col-md-1 hidden-sm hidden-xs"></div>
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<img src="./image/Custom-Delivery.png" class="img-responsive">
+</div>
+</div>
+</section>
+
+<section class="app">
+<div class="container">
+<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<h2>Download the Application Get 10% Discount </h2>
+<p>Our easy-to-use mobile app puts everything you need to manage your courier transactions </p>
+<a href="#" class="btn"><i class="fa fa-play" aria-hidden="true"></i> Google Play</a>
+<a href="#" class="btn"><i class="fa fa-apple" aria-hidden="true"></i> App Store</a>
+</div>
+</div>
+</section>
+
+
+<section class="logo">
+<div class="container">
+<div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+<img src="./image/acc-singapore-enterprise-moh.png" class="img-responsive">
+</div>
+<div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+<img src="./image/acc-singapore-industry-icon.png" class="img-responsive">
+</div>
+<div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+<img src="./image/acc-singapore-logistics-association 1.png" class="img-responsive">
+</div>
+<div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+<img src="./image/acc-singapore-premier-choice.png" class="img-responsive">
+</div>
+<div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+<img src="./image/acc-singapore-trade-quality.png" class="img-responsive">
+</div>
+<div class="col-lg-2 col-md-2 col-sm-6 col-xs-6">
+<img src="./image/acc-top-100-sme.png" class="img-responsive">
+</div>
+</div>
+</section>
+<?php
+include('footer.php');
+?>
